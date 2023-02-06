4a2bc42c447d7554077477c0edbbd6e59d508d36
jdaguilar <jorgeaguilar2761@gmail.com>
2023-02-06 05:28:54 -0500

    add github block and docker block

--- cohorts/2023/week_2_workflow_orchestration/flows/deployments/etl_parent_flow_deploy.py
+++ cohorts/2023/week_2_workflow_orchestration/flows/deployments/etl_parent_flow_deploy.py
@@ -1,11 +1,19 @@
 from prefect.deployments import Deployment
+from prefect.filesystems import GitHub
+from prefect.infrastructure.docker import DockerContainer
+
 
 from parametrized_url import etl_parent_flow
 
 
+github_block = GitHub.load("github-block")
+docker_container_block = DockerContainer.load("docker-executor")
+
 deployment  = Deployment.build_from_flow(
     flow=etl_parent_flow,
     name="Parametrized ETL",
+    storage=github_block,
+    infrastructure=docker_container_block,
 )
 
 
